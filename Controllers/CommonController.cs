52a041928dd12d3f6bf6c7e0f90fe9df0aa6cccf
Manognarayasam <manognarayasam1998@gmail.com>
2018-06-23 18:53:21 +0530

    Intial Commit

---
+++ Controllers/CommonController.cs
@@ -0,0 +1,23 @@
+﻿using Ezerkaproject.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Ezerkaproject.Controllers
+{
+    
+    public class CommonController : Controller
+    {
+
+        // GET: Common
+        public ActionResult About()
+        {
+
+            About model = new About();
+            return View(model);
+            
+        }
+    }
+}
